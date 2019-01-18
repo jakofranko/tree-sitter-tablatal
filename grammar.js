e8996ff87eca81828783c267e0f16478f908c90a
Jake Franklin <jacob.d.franklin@gmail.com>
2019-01-17 21:32:32 -0700

    Updates to the grammar and added tests

--- grammar.js
+++ grammar.js
@@ -2,15 +2,21 @@ module.exports = grammar({
   name: 'Tablatal',
 
   rules: {
-    // The production rules of the context-free grammar
-    source_file: $ => repeat($._definition),
-
-    _definition: $ => choice(
-        $.header_definition,
-        $.row_definition
+    source_file: $ => seq(
+        prec(1, $.header_definition),
+        repeat(prec(2, $.row_definition))
     ),
 
-    header_definition: $ => /[A-Z_\s]+\n/,
-    row_definition: $ => /.*\n/
+    header_definition: $ => seq(
+        repeat1($.column_definition),
+        /\n/
+    ),
+    column_definition: $ => /[A-Z\_]+/,
+
+    row_definition: $ => seq(
+        repeat($.item_definition),
+        /\n/
+    ),
+    item_definition: $ => /\S+/
   }
 });
